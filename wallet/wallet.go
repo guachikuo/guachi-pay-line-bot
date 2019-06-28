3bd873ccd3045ae120552e3b3ae72c8df5c37871
Andy Kuo <andykuo@17.media>
2019-06-28 23:43:50 +0800

    fix some namings, logics and add GetBalance

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -17,11 +17,12 @@ const (
 	`
 	patchWallet       = `UPDATE "UsersWallet" SET balance = $1 WHERE "userID" = $2;`
 	atomicPatchWallet = `UPDATE "UsersWallet" SET balance = balance + $1 WHERE "userID" = $2;`
+	getBalance        = `SELECT balance FROM "UsersWallet" WHERE "userID" = $1`
 )
 
 var (
-	// ErrWalletNotExist occurs when trying to do operation to the non-exist wallet
-	ErrWalletNotExist = fmt.Errorf("the wallet doesn't exist")
+	// ErrWalletNotFound occurs when trying to do operation to the non-exist wallet
+	ErrWalletNotFound = fmt.Errorf("the wallet doesn't exist")
 	// ErrWalletExist occurs when trying to create a wallet that has already been created
 	ErrWalletExist = fmt.Errorf("the wallet has already been created")
 )
@@ -34,10 +35,12 @@ type impl struct {
 type Wallet interface {
 	// Create creates a new wallet for user
 	Create(userID string) error
-	// Empty will empty user's wallet
-	Empty(userID string) error
+	// Empty will empty user's balance to zero
+	EmptyBalance(userID string) error
+	// GetBalance will get balance of a user
+	GetBalance(userID string) (int64, error)
 	// Deposit will deposit `amount` NTD to user's wallet
-	Deposit(userID string, amount int64) error
+	Deposit(userID string, amount int64, reason string) error
 	// Spend will spend `amount` NTD from user's wallet
 	Spend(userID string, amount int64, reason string) error
 }
@@ -54,17 +57,16 @@ func NewWallet() (Wallet, error) {
 	}, nil
 }
 
-// Create creates a new wallet for user
 func (im *impl) Create(userID string) error {
 	result, err := im.db.Exec(checkIfWalletExists, userID)
 	if err != nil {
-		logrus.WithField("err", err).Error("im.db.Exec checkIfWalletExists failed in InitWallet")
+		logrus.WithField("err", err).Error("im.db.Exec(checkIfWalletExists) failed in Create")
 		return err
 	}
 
 	rowsAffected, err := result.RowsAffected()
 	if err != nil {
-		logrus.WithField("err", err).Error("result.RowsAffected failed in InitWallet")
+		logrus.WithField("err", err).Error("result.RowsAffected failed in Create")
 		return err
 	}
 
@@ -74,36 +76,47 @@ func (im *impl) Create(userID string) error {
 
 	result, err = im.db.Exec(createWallet, userID, int64(0))
 	if err != nil {
-		logrus.WithField("err", err).Error("im.db.Exec upsert wallet failed in InitWallet")
+		logrus.WithField("err", err).Error("im.db.Exec(createWallet) failed in Create")
 		return err
 	}
 
 	if rowsAffected, err = result.RowsAffected(); err != nil || err == nil && rowsAffected == int64(0) {
-		logrus.WithField("err", err).Error("result.RowsAffected failed in InitWallet")
+		logrus.WithField("err", err).Error("result.RowsAffected failed in Create")
 		return err
 	}
 	return nil
 }
 
-// Empty will empty user's wallet
-func (im *impl) Empty(userID string) error {
+func (im *impl) EmptyBalance(userID string) error {
 	result, err := im.db.Exec(patchWallet, int64(0), userID)
 	if err != nil {
-		logrus.WithField("err", err).Error("im.db.Exec upsert wallet failed in InitWallet")
+		logrus.WithField("err", err).Error("im.db.Exec(patchWallet) failed in EmptyBalance")
 		return err
 	}
 
 	if rowsAffected, err := result.RowsAffected(); err != nil {
-		logrus.WithField("err", err).Error("result.RowsAffected failed in InitWallet")
+		logrus.WithField("err", err).Error("result.RowsAffected failed in EmptyBalance")
 		return err
 	} else if rowsAffected == int64(0) {
-		return ErrWalletNotExist
+		return ErrWalletNotFound
 	}
 	return nil
 }
 
-// Deposit will deposit `amount` NTD to user's wallet
-func (im *impl) Deposit(userID string, amount int64) error {
+func (im *impl) GetBalance(userID string) (int64, error) {
+	balance := int64(0)
+	if err := im.db.QueryRow(getBalance, userID).Scan(
+		&balance,
+	); err != nil && err != sql.ErrNoRows {
+		logrus.WithField("err", err).Error("im.db.Query(getBalance) failed in GetBalance")
+		return int64(0), err
+	} else if err == sql.ErrNoRows {
+		return int64(0), ErrWalletNotFound
+	}
+	return balance, nil
+}
+
+func (im *impl) Deposit(userID string, amount int64, reason string) error {
 	result, err := im.db.Exec(atomicPatchWallet, amount, userID)
 	if err != nil {
 		logrus.WithField("err", err).Error("im.db.Exec upsert wallet failed in AtomicPatchWallet")
@@ -114,12 +127,11 @@ func (im *impl) Deposit(userID string, amount int64) error {
 		logrus.WithField("err", err).Error("result.RowsAffected failed in AtomicPatchWallet")
 		return err
 	} else if err == nil && rowsAffected == int64(0) {
-		return ErrWalletNotExist
+		return ErrWalletNotFound
 	}
 	return nil
 }
 
-// Spend will spend `amount` NTD from user's wallet
 func (im *impl) Spend(userID string, amount int64, reason string) error {
 	result, err := im.db.Exec(atomicPatchWallet, -1*amount, userID)
 	if err != nil {
@@ -131,7 +143,7 @@ func (im *impl) Spend(userID string, amount int64, reason string) error {
 		logrus.WithField("err", err).Error("result.RowsAffected failed in AtomicPatchWallet")
 		return err
 	} else if err == nil && rowsAffected == int64(0) {
-		return ErrWalletNotExist
+		return ErrWalletNotFound
 	}
 	return nil
 }
