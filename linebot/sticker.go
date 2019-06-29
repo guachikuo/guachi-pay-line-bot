9f9191ffe67a05aad9a80c6ceb3c915d4a58a99d
Andy Kuo <andykuo@17.media>
2019-06-29 14:45:54 +0800

    add all stickers

--- linebot/sticker.go
+++ linebot/sticker.go
@@ -1,35 +1,56 @@
 package linebot
 
+import (
+	"strconv"
+
+	"github.com/sirupsen/logrus"
+)
+
 const (
 	// Brown, Cony & Sally
 	stickerPackageID1 = "11537"
 	// CHOCO & Friends
 	stickerPackageID2 = "11538"
+	// UNIVERSTAR BT21
+	stickerPackageID3 = "11539"
 )
 
 var (
 	// https://developers.line.biz/media/messaging-api/sticker_list.pdf
-	stickerList = map[string]map[string]string{
-		stickerPackageID1: map[string]string{
-			"52002735": "Cony is happy",
-			"52002758": "Cony makes a funny face",
-			"52002755": "Cony is begging for forgiveness",
-			"52002767": "Brown is angry",
-			"52002744": "Brown is confused",
-		},
-		stickerPackageID2: map[string]string{
-			"51626501": "Moon thumbs up",
-			"51626504": "Moon is laughing",
-			"51626522": "Moon is crying",
-			"51626511": "Boss is shocked",
-			"51626507": "Moon is blowing the trumpet",
-		},
-	}
+	stickerList = map[string]map[string]struct{}{}
 )
+
+func init() {
+	// https://developers.line.biz/media/messaging-api/sticker_list.pdf
+	stickerPackage := map[string]struct{}{}
+	for i := 52002734; i <= 52002770; i++ {
+		stickerPackage[strconv.FormatInt(int64(i), 10)] = struct{}{}
+	}
+	for i := 52002777; i <= 52002779; i++ {
+		stickerPackage[strconv.FormatInt(int64(i), 10)] = struct{}{}
+	}
+	stickerList[stickerPackageID1] = stickerPackage
+
+	stickerPackage = map[string]struct{}{}
+	for i := 51626494; i <= 51626533; i++ {
+		stickerPackage[strconv.FormatInt(int64(i), 10)] = struct{}{}
+	}
+	stickerList[stickerPackageID2] = stickerPackage
+
+	stickerPackage = map[string]struct{}{}
+	for i := 52114110; i <= 52114149; i++ {
+		stickerPackage[strconv.FormatInt(int64(i), 10)] = struct{}{}
+	}
+	stickerList[stickerPackageID3] = stickerPackage
+}
 
 func getSticker() (packageID, stickerID string) {
 	for packageID, stickers := range stickerList {
 		for stickerID := range stickers {
+			logrus.WithFields(logrus.Fields{
+				"packageID": packageID,
+				"stickerID": stickerID,
+			}).Info("sticker is selected")
 			return packageID, stickerID
 		}
 	}
